3ec97615d61116bfde03665ac6ed6100d7cf460d
giovanni-peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2024-01-26 21:12:42 -0300

    Create Start.bat

---
+++ Start.bat
@@ -0,0 +1,11 @@
+@echo off
+echo.
+echo ===============================
+echo =      1-Abrir Twich          =            
+echo =      2-Abrir Youtube        =                         
+echo ===============================
+echo.
+echo.
+
+set /p opcion = IntroduscaUnaOpcion:
+
