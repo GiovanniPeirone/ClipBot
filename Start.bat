170ba1bda54ee2816abe1ab7b4422221e904158d
giovanni-peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2024-01-31 22:38:12 -0300

    Update Start.bat

--- Start.bat
+++ Start.bat
@@ -10,12 +10,11 @@ echo =      2-Abrir Youtube        =
 echo ===============================
 echo.
 echo.
-set /p opcion= IntroduscaUnaOpcion:
+set /p opcion=IntroduscaUnaOpcion:
 
 IF %opcion% == 1 (
     start https://www.twitch.tv/directory
-)
-ELSE %opcion% == 2 (
+)ELSE %opcion% == 2 (
     start https://www.youtube.com
 )
 
